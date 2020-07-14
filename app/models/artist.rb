273d9ad1b9f1a79c3625100574c08d0a2895c59e
vagrant <vagrant@vagrant.vm>
2020-07-14 10:37:33 +0000

    誕生日登録用のメソッドをartistモデルに作成

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,10 @@
 class Artist < ApplicationRecord
   validates :name, presence: true
   validates :gender, presence: true
+
+  def set_birth_day(birth)
+    self.birth_year = birth.year
+    self.birth_month = birth.month
+    self.birth_day = birth.day
+  end
 end
