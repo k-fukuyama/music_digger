aef028667243b48f5690045e264bb9a141343811
vagrant <vagrant@vagrant.vm>
2020-07-23 08:31:32 +0000

    ジャンルテーブルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_10_071324) do
+ActiveRecord::Schema.define(version: 2020_07_23_032053) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 2020_06_10_071324) do
     t.integer "birth_day"
     t.integer "gender", null: false
     t.string "country"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
