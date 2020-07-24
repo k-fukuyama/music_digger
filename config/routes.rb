232472fbb1718aba62bb19c0385a13af9ac97f38
vagrant <vagrant@vagrant.vm>
2020-07-24 01:52:25 +0000

    genre用のルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   get 'artists/index'
   get 'artists/new'
+
+  resources :genres, only: [:index, :new, :edit]
+
   namespace :api, {format: 'json'} do
     namespace :v1 do
       resources :artists, only: [:index, :create]
+      resources :genres, only: [:index, :create]
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
