1dccf4c1a9cc2f9309ec8d49bd118d89fa68c13e
vagrant <vagrant@vagrant.vm>
2020-07-24 14:12:38 +0000

    アーティストモデルとジャンルモデルを関連付け

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,6 +2,9 @@ class Artist < ApplicationRecord
   validates :name, presence: true
   validates :gender, presence: true
 
+  has_many :artist_genres, dependent: :destroy
+  has_many :genres, through: :artist_genres
+
   def set_birth_day(birth)
     self.birth_year = birth.year
     self.birth_month = birth.month
