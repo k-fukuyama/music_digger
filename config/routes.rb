ef8a4eb624d50321bb11379aa4ae6c2c1d155a17
vagrant <vagrant@vagrant.vm>
2020-08-01 06:01:57 +0000

    discographiesのapiじゃない方のルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   resources :genres, only: [:index, :new, :edit]
   resources :songs, only: [:index, :new, :edit]
+  resources :discographies, only: [:index, :new, :edit]
 
   namespace :api, {format: 'json'} do
     namespace :v1 do
