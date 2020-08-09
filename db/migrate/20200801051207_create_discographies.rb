5f98f5c96fafda7ef8c44931ccce657b89f75111
vagrant <vagrant@vagrant.vm>
2020-08-09 00:32:17 +0000

    ディスコグラフィーテーブルにartist_idを入れ忘れていたので追加

--- db/migrate/20200801051207_create_discographies.rb
+++ db/migrate/20200801051207_create_discographies.rb
@@ -7,6 +7,7 @@ class CreateDiscographies < ActiveRecord::Migration[5.2]
       t.string :label
       t.text :explanation
       t.date :sales_start_at
+      t.integer :artist_id, null: false
       t.integer :grammy_id
       t.integer :genre_id
       t.integer :chart
