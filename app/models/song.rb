dec1bad8f9b8a6c452e3c14976e3ca0a1bac34b6
vagrant <vagrant@vagrant.vm>
2020-08-09 00:41:50 +0000

    アーティストを名前で検索して、アーティストのidをハッシュで返すメソッドを実装

--- app/models/song.rb
+++ app/models/song.rb
@@ -15,4 +15,31 @@ class Song < ApplicationRecord
       }
     )
   end
+
+  def fetch_some_artist_ids_of_hash(song_info)
+    artists = Artist.where(
+      name: [
+      song_info[:producer],
+      song_info[:composer],
+      song_info[:lyricist],
+      song_info[:vocalist],
+      song_info[:bassist],
+      song_info[:guitarist],
+      song_info[:drummer],
+      song_info[:keyboardist]
+      ]
+    ).select(:id, :name)
+
+    ids_of_hash = {}
+    artists.each do |artist|
+      ids_of_hash[:producer_id] = artist.id if song_info[:producer] == artist.name
+      ids_of_hash[:composer_id] = artist.id if song_info[:composer] == artist.name
+      ids_of_hash[:lyricist_id] = artist.id if song_info[:lyricist] == artist.name
+      ids_of_hash[:bassist_id] = artist.id if song_info[:bassist] == artist.name
+      ids_of_hash[:guitarist_id] = artist.id if song_info[:guitarist] == artist.name
+      ids_of_hash[:keyboardist_id] = artist.id if song_info[:keyboardist] == artist.name
+    end
+
+    ids_of_hash
+  end
 end
