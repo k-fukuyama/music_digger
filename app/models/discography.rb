3f7f56df965ed1d5f80f4ba6cc35daf6456c6621
vagrant <vagrant@vagrant.vm>
2020-08-09 06:54:41 +0000

    ディスコグラフィーの再生時間を登録するメソッドを定義

--- app/models/discography.rb
+++ app/models/discography.rb
@@ -1,2 +1,13 @@
 class Discography < ApplicationRecord
+  def set_play_length(songs)
+    min = 0
+    sec = 0
+
+    songs.each do |song|
+      min += song.min
+      sec += song.sec
+    end
+
+    self.play_length= Time.at((min * 60) + sec).utc.strftime('%X')
+  end
 end
