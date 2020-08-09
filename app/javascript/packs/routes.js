e1aa90a61efc3149b0fbcbf84f050b5b97a3f238
vagrant <vagrant@vagrant.vm>
2020-08-09 00:29:04 +0000

    routes.jsにディスコグラフィー登録ページのルーティングを追加

--- app/javascript/packs/routes.js
+++ app/javascript/packs/routes.js
@@ -4,6 +4,7 @@ import Index from './components/app.vue';
 import ArtistIndex from './components/artists/artists_index.vue';
 import ArtistNew from './components/artists/artists_new.vue';
 import GenresNew from './components/genres/genres_new.vue';
+import DiscographiesNew from './components/discographies/discographies_new.vue';
 
 Vue.use(VueRouter);
 
@@ -13,7 +14,9 @@ const router = new VueRouter({
     { path: '/', component: Index, name: 'root_path' },
     { path: '/artists/index', component: ArtistIndex, name: 'artists_index_path' },
     { path: '/artists/new',  component: ArtistNew },
-    { path: '/genres/new', component: GenresNew }
+    { path: '/genres/new', component: GenresNew },
+    { path: '/discographies/new', component: DiscographiesNew }
+
   ]
 });
 
