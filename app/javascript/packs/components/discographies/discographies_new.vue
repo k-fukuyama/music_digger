68e9f58ce61fc5df5ba3a143ffc5530a7d5c5ee8
vagrant <vagrant@vagrant.vm>
2020-08-10 01:59:04 +0000

    CSRFでエラーが起きない様に修正

--- app/javascript/packs/components/discographies/discographies_new.vue
+++ app/javascript/packs/components/discographies/discographies_new.vue
@@ -293,6 +293,9 @@
 <script>
   import axios from 'axios';
 
+  const token = document.getElementsByName("csrf-token")[0].getAttribute("content");
+  axios.defaults.headers.common["X-CSRF-Token"] = token;
+
   export default {
     data: function() {
       return {
