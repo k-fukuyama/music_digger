7afca3973138bedab58f5ee92c511c85fff1a5ce
vagrant <vagrant@vagrant.vm>
2020-11-14 05:54:08 +0000

    composerの部分がproducerになっていたので修正

--- app/models/song.rb
+++ app/models/song.rb
@@ -9,7 +9,7 @@ class Song < ApplicationRecord
   belongs_to :guitarist, class_name: 'Artist',   foreign_key: :guitarist_id, optional: true
   belongs_to :drummer, class_name: 'Artist',     foreign_key: :drummer_id, optional: true
   belongs_to :keyboardist, class_name: 'Artist', foreign_key: :keyboardist_id, optional: true
-  
+
   def set_song_params(song_info, artist_ids_of_hash = nil)
     artist_ids_of_hash = fetch_some_artist_ids_of_hash(song_info) unless artist_ids_of_hash.present?
     song_min = song_info[:min]
@@ -20,7 +20,7 @@ class Song < ApplicationRecord
         title: song_info[:title],
         track_number: song_info[:track_number],
         producer_id: artist_ids_of_hash[:producer_id],
-        composer_id: artist_ids_of_hash[:producer_id],
+        composer_id: artist_ids_of_hash[:composer_id],
         lyricist_id: artist_ids_of_hash[:lyricist_id],
         bassist_id: artist_ids_of_hash[:bassist_id],
         guitarist_id: artist_ids_of_hash[:guitarist_id],
