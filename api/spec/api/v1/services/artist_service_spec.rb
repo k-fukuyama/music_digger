48dfd4f2eb1ef3dea86c36080596efb75544a452
福山慶太 <fukuyamakeita@fukuyamakeitanoMacBook-Air.local>
2021-05-22 18:56:40 +0900

    アーティストサービスクラスのcreate!メソッドのテストを追加

--- api/spec/api/v1/services/artist_service_spec.rb
+++ api/spec/api/v1/services/artist_service_spec.rb
@@ -7,4 +7,13 @@ RSpec.describe V1::Services::ArtistService do
 
     it { is_expected.to eq target_artists }
   end
+
+  describe '.create!' do
+    subject { described_class.create!(params[:artist])}
+    let!(:params) { {artist: { name: 'name', gender: '男性', country: 'japan', birth: '1993-06-25' }} }
+
+    it 'creates artist' do
+      expect{ subject }.to change { Artist.count }.from(0).to(1)
+    end
+  end
 end
