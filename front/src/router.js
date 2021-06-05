ca533e717ed61b50bf60e467f3ba4917b73ac5b4
福山慶太 <fukuyamakeita@fukuyamakeitanoMacBook-Air.local>
2021-06-05 19:59:17 +0900

    ディスコグラフィーの登録ページを実装

--- front/src/router.js
+++ front/src/router.js
@@ -4,7 +4,7 @@ import ArtistIndex from './components/artists/artists_index.vue';
 import ArtistNew from './components/artists/artists_new.vue';
 import GenresNew from './components/genres/genres_new.vue';
 // import GenresEdit from './components/genres/genres_edit.vue';
-// import DiscographiesNew from './components/discographies/discographies_new.vue';
+import DiscographiesNew from './components/discographies/discographies_new.vue';
 // import DiscographiesEdit from './components/discographies/discographies_edit.vue';
 // import GrammiesNew from './components/grammies/grammies_new.vue';
 
@@ -16,7 +16,7 @@ const router = new VueRouter({
     { path: '/artists/index', component: ArtistIndex, name: 'artists_index_path' },
     { path: '/artists/new',  component: ArtistNew },
     { path: '/genres/new', component: GenresNew },
-    // { path: '/discographies/new', component: DiscographiesNew },
+    { path: '/discographies/new', component: DiscographiesNew },
     // { path: '/discographies/:id/edit', component: DiscographiesEdit },
     // { path: '/grammies/new', component: GrammiesNew },
     // { path: '/genres/:id/edit', component: GenresEdit }
