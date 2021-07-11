5d206b9e56e8a15add158484e919b5f9299fe999
福山慶太 <fukuyamakeita@fukuyamakeitanoMacBook-Air.local>
2021-07-11 17:07:06 +0900

    ディスコグラフィーを検索をできるようにした

--- api/app/api/v1/discographies_api.rb
+++ api/app/api/v1/discographies_api.rb
@@ -1,6 +1,11 @@
 module V1
   class DiscographiesApi < Grape::API
     resources :discographies do
+      desc 'get discographies'
+      get do
+        present Services::DiscographyService.fetch_discographies(params[:search_word]), with: Entities::DiscographyEntity
+      end
+
       desc 'pots discography'
       post do
         Services::DiscographyService.create!(params[:discography], params[:song_infos], params[:grammy_flg])
